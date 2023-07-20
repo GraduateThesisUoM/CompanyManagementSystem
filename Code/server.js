dca0495c1da5a3153871041f49e86873465b24fb
Nektatios Papagalakis <nektarios00007@gmail.com>
2023-07-20 13:21:48 +0300

    n

--- Code/server.js
+++ Code/server.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-  })
+app.use(express.urlencoded({ extended: false }));
 
+/*--------   INDEX */
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+/*--------   LOG IN */
 app.get('/log_in', (req, res) => {
-    res.render('log_in.ejs')
-  })
+  res.render('log_in.ejs')
+})
+
+/*--------   SING UP */
+app.get('/sing_up', (req, res) => {
+  res.render('sing_up.ejs')
+})
+
+app.post('/sing_up', async (req, res) => {
+  console.log(req.body.firstname)
+  /*try {
+    console.log(req.body.firstname)
+    res.redirect('/log_in')
+  } catch {
+    res.redirect('/sing_up?error')
+  }*/
+})
 
 app.listen(3000)
