c0e4fb3f916016228d3a72635fba675c45bb935d
DESKTOP-JDB71NO\nekta <nektarios00007@gmail.com>
2024-02-19 05:32:55 +0200

    ....

--- Code/routes/CreateRoutes.js
+++ Code/routes/CreateRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //Models
+const Warehouse = require("../Schemas/Warehouse");
 const Notification = require("../Schemas/Notification");
 
 //Authentication Function
@@ -11,6 +12,21 @@ const Authentication = require("../AuthenticationFunctions");
 router.get('/', Authentication.checkAuthenticated, async (req, res) => {
     res.render('accountant_pages/create_page.ejs', {user: req.user,
       notification_list: await Notification.find({$and:[{user_id: req.user.id} , {status: "unread"}]})});
+});
+
+  router.post('/warehouse', Authentication.checkAuthenticated, async (req, res) => {
+    try{
+      const warehouse = new Warehouse({
+        title: req.body.warehouse_title,
+        location: req.body.warehouse_location
+      });
+      // Save the new user to the database
+    await warehouse.save();
+    }
+    catch (err) {
+      console.error('Error deleting account:', err);
+      res.redirect('/error?origin_page=delete-account&error='+err);
+    }
 });
 
 module.exports = router;
