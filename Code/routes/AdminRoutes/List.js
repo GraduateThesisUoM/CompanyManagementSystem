d0d6882b3467ed2f4eb9dd31be2c0a1da7a6f1c6
DESKTOP-JDB71NO\nekta <nektarios00007@gmail.com>
2024-07-20 01:10:15 +0300

    list page

--- Code/routes/AdminRoutes/List.js
+++ Code/routes/AdminRoutes/List.js
@@ -24,26 +24,26 @@ router.get('/', Authentication.checkAuthenticated, async (req,res)=>{
     try{
         if(generalFunctions.checkAccessRigts(req,res)){
             var list_items = [];
+            var column_titles = [];
             if(req.query.searchfor == "companys"){
                 list_items = await Company.find();
                 list_items = list_items.map(item => ({
-                    name: item.name,
-                    status: item.status,
-                    registrationDate: formatDate(item.registrationDate)
+                    data :[ item.name, formatDate(item.registrationDate), item.status]
                 }));
+                column_titles = ["Name", "Reg Date","Status"]
             }
             else if (req.query.searchfor == "users"){
                 list_items = await User.find();
                 list_items = list_items.map(item => ({
-                    name: item.firstName,
-                    status: item.account_status,
-                    registrationDate: formatDate(item.registrationDate)
+                    data :[ item.firstName,item.lastName, formatDate(item.registrationDate), item.account_status]
                 }));
+                column_titles = ["First Name","Last Name","Reg Date","Status"]
             }
             
             var data = {
                 user: req.user,
-                list_items : list_items
+                list_items : list_items,
+                column_titles : column_titles
             };
             res.render('admin_pages/list.ejs', data)
           }
@@ -66,5 +66,7 @@ const formatDate = (dateString) => {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     return `${day}/${month}/${year} ${hours}:${minutes}`;
 };
+
+
 
 module.exports = router;
